c0f2348637ebc44cd659a54f3fc7b8670a2abb9c
Mayur1009 <mayur.k.shende@gmail.com>
2024-06-22 19:30:13 +0200

    Quiz 2

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let t = input.trim();
+    t.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+    format!("{} world!", input)
+    // let mut a: String = String::from(input);
+    // let b: &str = " world!";
+    // a.push_str(b);
+    // return a;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
